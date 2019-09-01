4a6ca4dde7613fa6589f8820535f43b8e352c493
nicholas-schaefere <nschaefe@optonline.net>
2019-09-01 14:24:21 -0400

    removed excess comments from API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,10 @@
 import axios from "axios";
-const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
-const AUTHORSEARCH = "inauthor:"
-const ORDERING = "&orderBy=newest"
-const MAXRESULTS = "&maxResults=40"
-const APIKEY = "&key=AIzaSyBvrAqrUn3DQqWapuse08m421Df1ZU5ZcU";
 
 export default {
   // Gets all songs
   getSongs: function(loggedInUserId) {
   return axios.get("/api/tracks/" + "/?q=" + loggedInUserId);
   },
-  // // Gets all songs
-  // getSongs: function() {
-  // return axios.get("/api/tracks");
-  // },
   // Saves track to the database
   banSong: function(trackData) {
   return axios.post("/api/tracks", trackData);
@@ -22,7 +13,4 @@ export default {
   deleteSong: function(id) {
     return axios.delete("/api/tracks/" + id);
   }
-  // search: function(query) {
-  //   return axios.get(`${BASEURL}${AUTHORSEARCH}${query}${ORDERING}${MAXRESULTS}${APIKEY}`);
-  // },
 };
